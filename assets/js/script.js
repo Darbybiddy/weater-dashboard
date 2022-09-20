0e5e2706f1a6f73871e129a0f5894fbc19369025
Darby Biddy <darbybiddy3@gmail.com>
2022-09-20 17:53:20 -0500

    made changes to the HTML, added cards

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// create a current date variable that displays the current dates
+// add variable for the search text
+// add variable for the display section.
+// add a click event to the search button
+// add the weather api doc
+// add icons that go with the correct weather conditions
+// add a variable for the color change for UV index
+// in the display section i need to have: temp, wind, humidity, uv index, icon.
+// in the forecast i need to have: future date, temp, wind, humidity, icon.
+
+
+var currentDate = moment().format("MM/DD/YYYY");
+var cityInfo
+var citySearch
