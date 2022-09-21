dca1fcc3975b5e1d287732799c5c760b5bc883e4
Darby Biddy <darbybiddy3@gmail.com>
2022-09-21 10:01:05 -0500

    added javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,51 @@
 
 var currentDate = moment().format("MM/DD/YYYY");
 var cityInfo
-var citySearch
+var citySearch = document.querySelector("#city-names")
+var searchButton = document.querySelector(".search-btn")
+var titleWeather = document.querySelector("#current-date")
+titleWeather.textContent = currentDate
+
+var apiKey = "2c7427a61257c9ff1af20d7fb03dd9bf"
+
+function getCurrentWeather (city){
+    console.log(city)
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+fetch (url)
+.then ((res)=>{
+    return res.json();
+})
+.then ((data)=>{
+
+    console.log(data)
+    var lat = data.coord.lat
+    var lon = data.coord.lon
+getOneCall(lat,lon)
+})
+}
+
+function getOneCall (lat,lon){
+    console.log(lat,lon)
+
+    var url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={minutely,hourly,alerts}&appid=${apiKey}&units=imperial`
+    fetch (url)
+    .then ((res)=>{
+        return res.json();
+    })
+    .then ((data)=>{
+        console.log(data)
+        //Add two functions - one to display the current weathher another to display the forecast
+        //call the fucntion here
+    })
+}
+// declare the fucntions here
+
+searchButton.addEventListener("click", (event)=>{
+    event.preventDefault()
+    var userInput = citySearch.value
+    
+    getCurrentWeather(userInput)
+});
+
+
+
