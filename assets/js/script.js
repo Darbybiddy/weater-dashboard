d9c71b64553f2cbd88f5356c92ca80565544646a
Darby Biddy <darbybiddy3@gmail.com>
2022-09-26 15:40:25 -0500

    modified the js and index and css files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,77 @@
-// create a current date variable that displays the current dates
-// add variable for the search text
-// add variable for the display section.
-// add a click event to the search button
-// add the weather api doc
-// add icons that go with the correct weather conditions
-// add a variable for the color change for UV index
-// in the display section i need to have: temp, wind, humidity, uv index, icon.
-// in the forecast i need to have: future date, temp, wind, humidity, icon.
-
-
 var currentDate = moment().format("MM/DD/YYYY");
-var cityInfo
-var citySearch = document.querySelector("#city-names")
-var searchButton = document.querySelector(".search-btn")
-var titleWeather = document.querySelector("#current-date")
-titleWeather.textContent = currentDate
-var displayInfoEl = document.querySelector(".city-info")
+
+var citySearch = document.querySelector("#city-names");
+var searchButton = document.querySelector(".search-btn");
+var titleWeather = document.querySelector("#current-date");
+titleWeather.textContent = currentDate;
+var displayInfoEl = document.querySelector(".city-info");
 var userInput;
-var apiKey = "2c7427a61257c9ff1af20d7fb03dd9bf"
+var apiKey = "2c7427a61257c9ff1af20d7fb03dd9bf";
+var currentTempEl = document.getElementById("current-temp");
+var currentWindEl = document.getElementById("current-windspeed");
+var currentHumidityEl = document.getElementById("current-humidity");
+var currentCityNameEl = document.getElementById("current-name");
+var currentIconEl = document.getElementById("current-icon");
 
-function getCurrentWeather (city){
-    console.log(city)
-var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
-fetch (url)
-.then ((res)=>{
-    return res.json();
-})
-.then ((data)=>{
+var dayOneTempEl = document.getElementById("day-one-temp");
+var dayOneWindEl = document.getElementById("day-one-wind");
+var dayOneHumidityEl = document.getElementById("day-one-humidity");
+var dayOneIconEl = document.getElementById("day-one-icon");
 
-    console.log(data)
-    var lat = data.coord.lat;
-    var lon = data.coord.lon;
-getOneCall(lat,lon)
-})
+function getCurrentWeather(city) {
+  console.log(city);
+  var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      displayInfo(data);
+      getOneCall(lat, lon);
+    });
 }
 
-function displayInfo(data){
-    var cityNameEl = document.createElement("h2");
-    cityNameEl.textContent = userInput; 
-   var tempEl = document.createElement("p");
-   tempEl.textContent = data.main.temp
-   console.log(data.main.temp)
+function displayInfo(data) {
+  currentCityNameEl.textContent = userInput;
+  currentTempEl.textContent = `Temp: ${data.main.temp} F`;
+  console.log(data.main.temp);
+  currentHumidityEl.textContent = `Humidity: ${data.main.humidity} %`;
+  currentWindEl.textContent = `wind: ${data.wind.speed} MPH`;
 
-    displayInfoEl.append(cityNameEl);
+  currentIconEl.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 }
+function displayForecast(data) {
+  for (i = 0; i < data; i++) {
+    // need to add it to every card, so that i dont have to keep adding the same code for each card.
+  }
 
-function getOneCall (lat,lon){
-    console.log(lat,lon)
+  dayOneTempEl.textContent = `Temp: ${data.list[0].main.temp} F`;
+  dayOneHumidityEl.textContent = `Humidity: ${data.list[0].main.humidity} %`;
+  dayOneWindEl.textContent = `wind: ${data.list[0].wind.speed} MPH`;
 
-    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={minutely,hourly,alerts}&appid=${apiKey}&units=imperial`
-    fetch (url)
-    .then ((res)=>{
-        return res.json();
+  dayOneIconEl.src = `http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
+}
+function getOneCall(lat, lon) {
+  console.log(lat, lon);
+
+  var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&exclude={minutely,hourly,alerts}&appid=${apiKey}&units=imperial`;
+  fetch(url)
+    .then((res) => {
+      return res.json();
     })
-    .then ((data)=>{
-        console.log(data);
-        displayInfo(data);
-        //Add two functions - one to display the current weathher another to display the forecast
-        //call the fucntion here
-    })
+    .then((data) => {
+      console.log(data);
+      displayForecast(data);
+    });
 }
 // declare the fucntions here
 
+searchButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  userInput = citySearch.value;
 
-searchButton.addEventListener("click", (event)=>{
-    event.preventDefault()
-    userInput = citySearch.value
-    
-    getCurrentWeather(userInput)
+  getCurrentWeather(userInput);
 });
-
